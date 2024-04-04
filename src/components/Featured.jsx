6a8cefbb2819ec9ce68145107cd3a724a624b4d9
Rennerx3 <rmokosce21@hotmail.com>
2024-04-04 10:11:51 -0300

    slider done

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -5,11 +5,17 @@ import { useEffect, useState } from 'react';
 const Featured = ({data}) => {
     const [renderTitle, setRenderTitle] = useState(false);
     const [loader, setLoader] = useState(true);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
     const firstThreeMovies = data.slice(0,3);
-    console.log(firstThreeMovies);
+
+    console.log(currentIndex);
 
     useEffect(() => {
+        if(firstThreeMovies.length > 0){
+            setLoader(false);
+        }
+
         const handleWidth = () => {
             const width = window.innerWidth;
             if(width < 950){
@@ -26,54 +32,59 @@ const Featured = ({data}) => {
         return () => {
             window.removeEventListener('resize', handleWidth);
         }
-    },[]);
+    },[data]);
 
+    const handlePrev = () => {
+        setCurrentIndex(prevIndex => (prevIndex > 0 ? prevIndex -1 : firstThreeMovies.length - 1));
+    }
+
+    const handleNext = () => {
+        setCurrentIndex(prevIndex => (prevIndex < firstThreeMovies.length -1 ? prevIndex + 1 : 0));
+    }
 
     return ( 
         <main className="featured">
             <div className="backdrop">
                 <div className="sub-featured">
-                    <div className="image">
-                        <div className="punctuation">
-                            <FontAwesomeIcon icon={faStar} style={{color: "#ffffff", height: '17px', width: '17px'}} />
-                            <span><strong style={{fontSize: '18px'}}>8</strong>/10</span>
-                            <p style={{margin: '0', fontWeight: 'bold'}}>IMDB</p>
-                        </div>
-                        <img src="https://play-lh.googleusercontent.com/5efxbApfVAa9R1fStcVvSHqDhyXI40jLCd78b2NEuqNcC0McWceLAQGYnz0rFKZz7lywTdKNzfsXidP97A" alt="John Wick 4" />
-                        {renderTitle && <h1 style={{fontSize: '2.5rem'}}>John Wick 4</h1>}
-                    </div>
-                    <div className="text-icons">
-                        <div className="text">
-                            <h1>John Wick 4</h1>
-                            <hr />
-                            <p>
-                            John Wick 4 comienza con el personaje del título (Keanu Reeves)
-                            huyendo de nuevo mientras tiene a cientos de asesinos pisándole los talones.
-                            </p>
-                            <p>El villano principal de la serie es el Marqués de Gramont (Bill Skarsgård), 
-                            un líder de la Alta Mesa que sigue aumentando la recompensa por la cabeza de 
-                            Wick mientras también arregla algunos asuntos dentro de la organización, incluyendo
-                            la posible eliminación de Winston Scott (Ian McShane) 
-                            y algunas de las decisiones que tomó en el pasado.</p>
-                        </div>
-                        <div className="icons">
-                            <div>
-                                <div></div>
-                                <i className="fa-brands fa-square-youtube" style={{color: "#ffffff"}}></i>
-                                <p>Ver trailer</p>
+                    {loader ? (<div>Cargando...</div>) : 
+                    firstThreeMovies.map((el, index) => (
+                        (<div key={el.id} className={currentIndex === index ? 'main-div active' : 'main-div'}>
+                            <div className="image">
+                                <div className="punctuation">
+                                    <FontAwesomeIcon icon={faStar} style={{color: "#ffffff", height: '17px', width: '17px'}} />
+                                    <span><strong style={{fontSize: '18px'}}>{el.punctuation}</strong>/10</span>
+                                    <p style={{margin: '0', fontWeight: 'bold'}}>IMDB</p>
+                                </div>
+                                <img src={el.poster_img} alt="John Wick 4" />
+                                {renderTitle && <h1 style={{fontSize: '2.5rem'}}>{el.name}</h1>}
                             </div>
-                            <div>
-                                <div style={{height: '60px',width: '85px',  position: 'absolute', backgroundColor: '#554F95', borderRadius: '6px', top: '-4px', left: '13px', zIndex: '-1'}}></div>
-                                <FontAwesomeIcon icon={faTicket} style={{color: "#ffffff"}} />
-                                <p>Comprar ticket</p>
+                            <div className="text-icons">
+                                <div className="text">
+                                    <h1>{el.name}</h1>
+                                    <hr />
+                                    <p>{el.sinopsis}</p>
+                                </div>
+                                <div className="icons">
+                                    <div>
+                                        <div></div>
+                                        <i className="fa-brands fa-square-youtube" style={{color: "#ffffff"}}></i>
+                                        <p>Ver trailer</p>
+                                    </div>
+                                    <div>
+                                        <div style={{height: '60px',width: '85px',  position: 'absolute', backgroundColor: '#554F95', borderRadius: '6px', top: '-4px', left: '13px', zIndex: '-1'}}></div>
+                                        <FontAwesomeIcon icon={faTicket} style={{color: "#ffffff"}} />
+                                        <p>Comprar ticket</p>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
+                        </div>)
+                    ))
+                    }
                     <div className="pointers">
-                        <button>
+                        <button onClick={() => handlePrev()}>
                             <img src="../../public/arrow.png" alt="" />
                         </button>
-                        <button>
+                        <button onClick={() => handleNext()}>
                             <img src="../../public/arrow.png" alt="" />
                         </button>
                     </div>
